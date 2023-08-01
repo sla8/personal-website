52e0ee1d84e8e2093961faa211568d82b0fe4282
Silvia <sla>
2023-08-01 15:49:42 +0200

    refactoring

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,32 +2,31 @@ import { Component } from 'react';
 import { Link } from 'react-scroll';
 import logo from 'images/logo-silviala.svg';
 import { id as helloId } from 'res/landing/hello';
-
-interface NavbarLinkInput {
-  key: string;
-  to: string;
-  text: string;
-}
+import {
+  id as educationId,
+  title as educationTitle,
+} from 'res/landing/education';
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav className="fixed top-0 left-0 right-0 bg-black">
         <div className="w-full max-w-4xl flex flex-row items-center m-auto pr-10 pl-10">
-          <this.NavbarLogo />
+          {this.createNavbarLogo()}
           <div className="flex flex-row items-center justify-center w-full h-full">
-            <this.NavbarLink
-              key={'test'}
-              to={'test-section'}
-              text={'Lorem ipsum'}
-            />
+            {this.createNavbarLink(
+              `navbar-${educationId}`,
+              educationId,
+              educationTitle
+            )}
+            {this.createNavbarLink('test', 'test-section', 'Lorem ipsum')}
           </div>
         </div>
       </nav>
     );
   }
 
-  private NavbarLogo() {
+  private createNavbarLogo() {
     return (
       <Link
         key="navbar-logo"
@@ -44,21 +43,21 @@ export default class Navbar extends Component {
     );
   }
 
-  private NavbarLink(props: NavbarLinkInput) {
+  private createNavbarLink(key: string, to: string, text: string) {
     return (
       <Link
-        key={props.key}
+        key={key}
         className="
         flex h-full items-center pl-6 pr-6 pt-3 pb-3
         text-base font-light uppercase text-textwhite hover:border-t-8 hover:pt-1 hover:border-darkblue cursor-pointer"
         activeClass=""
-        to={props.to}
+        to={to}
         spy={true}
         smooth={true}
         offset={-64}
         duration={500}
       >
-        {props.text}
+        {text}
       </Link>
     );
   }
