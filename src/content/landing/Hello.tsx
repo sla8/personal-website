c27f230a8f0efd78a7a8f5a2810225c7d364683f
Silvia <sla>
2023-08-01 14:17:10 +0200

    add link to linkedin and github

--- src/content/landing/Hello.tsx
+++ src/content/landing/Hello.tsx
@@ -10,6 +10,8 @@ import {
 } from 'res/landing/hello';
 import womanReading from 'images/woman_web.svg';
 import { LandingSectionInput } from 'content/Landing';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Hello(props: LandingSectionInput) {
   return (
@@ -25,6 +27,8 @@ function createHelloContent(dark: boolean) {
   let textContent = (
     <div className="w-full pt-12 sm:pt-0 sm:ml-10 md:pt-0 md:ml-20">
       {createTextSectionContent(dark, helloTitle, helloSubtitle, helloText)}
+      <br />
+      {createSocialSubsection(dark)}
     </div>
   );
   let imageContent = (
@@ -33,4 +37,20 @@ function createHelloContent(dark: boolean) {
     </div>
   );
   return createSplitSectionContent(imageContent, textContent);
+}
+
+function createSocialSubsection(dark: boolean) {
+  return (
+    <div
+      className={`text-center ${dark ? 'text-textwhite' : 'text-textblack'}`}
+    >
+      <a href="https://www.linkedin.com/in/sla8">
+        <LinkedInIcon sx={{ fontSize: { xs: 30, sm: 30 } }} />
+      </a>
+      &nbsp;&nbsp;
+      <a href="https://github.com/sla8">
+        <GitHubIcon sx={{ fontSize: { xs: 25, sm: 25 } }} />
+      </a>
+    </div>
+  );
 }
